48aebb43e7cd2a0cbe9191f497e18e859559fa94
ByoungYong Kim <byoungyong@tiqets.com>
2022-03-16 23:28:08 +0100

    Fix the bug in vpnserver-start

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # SoftEtherVPN-docker
 
 ## How To Use
-The following command will run the SoftEtherVPN server and create a hub called `VPN` (hard-coded) with the password `hubpassword`, with the two users with the user name and password combo `user1` / `pass1` and `user2` / `pass2`
+The following command will run the SoftEtherVPN server where you can login with the two user name and password combo `user1` / `pass1` and `user2` / `pass2`, and the secret `secret`.
 ```
-docker run --env PASSWORD=hubpassword --env USERS="user1:pass1 user2:pass2" -p 500:500/udp -p 1701:1701/udp -p 4500:4500/udp ryubro/softether-vpn
+docker run --env PASSWORD=secret --env USERS="user1:pass1 user2:pass2" -p 500:500/udp -p 1701:1701/udp -p 4500:4500/udp ryubro/softether-vpn
 ```
--- vpnserver-start.sh
+++ vpnserver-start.sh
@@ -5,13 +5,15 @@ until vpncmd /server localhost /cmd HubList
 do
 	sleep 0.1s
 done
-vpncmd /server localhost /cmd HubCreate VPN /PASSWORD $PASSWORD
+vpncmd /server localhost /cmd HubCreate VPN /PASSWORD
+vpncmd /server localhost /cmd IPSecEnable /L2TP:yes /L2TPRAW:no /ETHERIP:no /PSK:$SECRET /DEFAULTHUB:VPN
+vpncmd /server localhost /hub:VPN /cmd SecureNatEnable
 for PAIR in $USERS
 do
-	vpncmd /server localhost /hub:VPN /password $PASSWORD /cmd UserCreate ${PAIR%:*} /GROUP:none /REALNAME:none /NOTE:none
-	vpncmd /server localhost /hub:VPN /password $PASSWORD /cmd UserPasswordSet ${PAIR%:*} /PASSWORD:${PAIR#*:}
+	vpncmd /server localhost /hub:VPN /cmd UserCreate ${PAIR%:*} /GROUP:none /REALNAME:none /NOTE:none
+	vpncmd /server localhost /hub:VPN /cmd UserPasswordSet ${PAIR%:*} /PASSWORD:${PAIR#*:}
 done
-vpncmd /server localhost /hub:VPN /password $PASSWORD /cmd UserList
+vpncmd /server localhost /hub:VPN /cmd UserList
 
 while true
 do
